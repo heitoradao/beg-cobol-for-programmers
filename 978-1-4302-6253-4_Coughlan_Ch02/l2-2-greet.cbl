843169c0a2d6f14264338e5cc4e4c9186ddf396e
F3952579 Heitor Jr <heitor.junior@bb.com.br>
2023-12-16 10:08:12 -0300

    IBM style

--- 978-1-4302-6253-4_Coughlan_Ch02/l2-2-greet.cbl
+++ 978-1-4302-6253-4_Coughlan_Ch02/l2-2-greet.cbl
@@ -1,14 +1,17 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID. CobolGreeting.
-*>Program to display COBOL greetings 
-DATA DIVISION.
-WORKING-STORAGE SECTION.
-01  IterNum   PIC 9 VALUE 5.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CobolGreeting.
+      *>Program to display COBOL greetings 
 
-PROCEDURE DIVISION.
-BeginProgram.
-   PERFORM DisplayGreeting IterNum TIMES.
-   STOP RUN.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  IterNum   PIC 9 VALUE 5.
+
+       PROCEDURE DIVISION.
+       BeginProgram.
+           PERFORM DisplayGreeting IterNum TIMES
+           STOP RUN
+           .
    
-DisplayGreeting.
-   DISPLAY "Greetings from COBOL".
+       DisplayGreeting.
+           DISPLAY "Greetings from COBOL"
+           .
