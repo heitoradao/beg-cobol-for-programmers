77099aed42a5a2ae9e1a72041d9bbbae8b54f88c
F3952579 Heitor Jr <heitor.junior@bb.com.br>
2024-03-06 13:58:18 -0300

    Fix listing 5-6
    - Fix identation.
    - Fix filename case.

--- 978-1-4302-6253-4_Coughlan_Ch05/Listing5-6.cbl
+++ 978-1-4302-6253-4_Coughlan_Ch05/Listing5-6.cbl
@@ -1,32 +1,34 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID. Listing5-6. 
-AUTHOR. Michael Coughlan.
-ENVIRONMENT DIVISION.
-INPUT-OUTPUT SECTION.
-FILE-CONTROL.
-   SELECT StudentFile ASSIGN TO "Listing5-6-TData.Dat" 
-    ORGANIZATION IS LINE SEQUENTIAL.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Listing5-6. 
+       AUTHOR. Michael Coughlan.
 
-DATA DIVISION.
-FILE SECTION.
-FD StudentFile.
-01 StudentDetails.
-   88  EndOfStudentFile  VALUE HIGH-VALUES.
-   02  StudentId       PIC X(8). 
-   02  StudentName     PIC X(25).
-   02  CourseCode      PIC X(5). 
+	   ENVIRONMENT DIVISION.
+	   INPUT-OUTPUT SECTION.
+	   FILE-CONTROL.
+		   SELECT StudentFile ASSIGN TO "Listing5-6-TData.dat" 
+			ORGANIZATION IS LINE SEQUENTIAL.
 
-PROCEDURE DIVISION.
-Begin.
-   OPEN INPUT StudentFile 
-   READ StudentFile 
-      AT END SET EndOfStudentFile TO TRUE
-   END-READ
-   PERFORM UNTIL EndOfStudentFile 
-      DISPLAY StudentName SPACE StudentId SPACE CourseCode 
-      READ StudentFile 
-         AT END SET EndOfStudentFile TO TRUE
-      END-READ 
-   END-PERFORM
-   CLOSE StudentFile 
-   STOP RUN.
+       DATA DIVISION.
+       FILE SECTION.
+       FD StudentFile.
+       01 StudentDetails.
+           88  EndOfStudentFile  VALUE HIGH-VALUES.
+           02  StudentId       PIC X(8). 
+           02  StudentName     PIC X(25).
+           02  CourseCode      PIC X(5). 
+
+       PROCEDURE DIVISION.
+       Begin.
+           OPEN INPUT StudentFile 
+           READ StudentFile 
+               AT END SET EndOfStudentFile TO TRUE
+           END-READ
+           PERFORM UNTIL EndOfStudentFile 
+               DISPLAY StudentName SPACE StudentId SPACE CourseCode 
+               READ StudentFile 
+                   AT END SET EndOfStudentFile TO TRUE
+               END-READ 
+           END-PERFORM
+           CLOSE StudentFile 
+           STOP RUN
+           .
