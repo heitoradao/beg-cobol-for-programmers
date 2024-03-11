06b8c097d4b6654d3398c23db1361870180b0f6a
F3952579 Heitor Jr <heitor.junior@bb.com.br>
2024-03-11 14:04:09 -0300

    identation & rtrim

--- 978-1-4302-6253-4_Coughlan_Ch05/Listing5-6.cbl
+++ 978-1-4302-6253-4_Coughlan_Ch05/Listing5-6.cbl
@@ -1,34 +1,34 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. Listing5-6. 
+       PROGRAM-ID. Listing5-6.
        AUTHOR. Michael Coughlan.
 
-	   ENVIRONMENT DIVISION.
-	   INPUT-OUTPUT SECTION.
-	   FILE-CONTROL.
-		   SELECT StudentFile ASSIGN TO "Listing5-6-TData.dat" 
-			ORGANIZATION IS LINE SEQUENTIAL.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+		   SELECT StudentFile ASSIGN TO "Listing5-6-TData.dat"
+			     ORGANIZATION IS LINE SEQUENTIAL.
 
        DATA DIVISION.
        FILE SECTION.
        FD StudentFile.
        01 StudentDetails.
            88  EndOfStudentFile  VALUE HIGH-VALUES.
-           02  StudentId       PIC X(8). 
+           02  StudentId       PIC X(8).
            02  StudentName     PIC X(25).
-           02  CourseCode      PIC X(5). 
+           02  CourseCode      PIC X(5).
 
        PROCEDURE DIVISION.
        Begin.
-           OPEN INPUT StudentFile 
-           READ StudentFile 
+           OPEN INPUT StudentFile
+           READ StudentFile
                AT END SET EndOfStudentFile TO TRUE
            END-READ
-           PERFORM UNTIL EndOfStudentFile 
-               DISPLAY StudentName SPACE StudentId SPACE CourseCode 
-               READ StudentFile 
+           PERFORM UNTIL EndOfStudentFile
+               DISPLAY StudentName SPACE StudentId SPACE CourseCode
+               READ StudentFile
                    AT END SET EndOfStudentFile TO TRUE
-               END-READ 
+               END-READ
            END-PERFORM
-           CLOSE StudentFile 
+           CLOSE StudentFile
            STOP RUN
            .
