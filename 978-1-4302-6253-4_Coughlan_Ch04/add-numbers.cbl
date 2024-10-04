e7562946bde7b3225cb7d8f1b470a1e3c93572c4
F3952579 Heitor Jr <heitor.junior@bb.com.br>
2024-10-03 21:24:58 -0300

    Cosmetic

--- 978-1-4302-6253-4_Coughlan_Ch04/add-numbers.cbl
+++ 978-1-4302-6253-4_Coughlan_Ch04/add-numbers.cbl
@@ -9,14 +9,16 @@
        01  Num1         PIC 9  VALUE 5.
        01  Num2         PIC 9  VALUE 4.
        01  Result       PIC 99 VALUE ZEROS.
-  
+
        PROCEDURE DIVISION.
        CalculateResult.
            DISPLAY "Enter a single digit number - " WITH NO ADVANCING
            ACCEPT Num1
            DISPLAY "Enter a single digit number - " WITH NO ADVANCING
            ACCEPT Num2
-           MULTIPLY Num1 BY Num2 GIVING Result  
+
+           ADD Num1 Num2 GIVING Result
+      *      MULTIPLY Num1 BY Num2 GIVING Result
            DISPLAY "Result is = ", Result
            STOP RUN
            .
